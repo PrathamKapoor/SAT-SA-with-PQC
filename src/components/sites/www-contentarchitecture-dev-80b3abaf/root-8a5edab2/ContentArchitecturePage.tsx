32b88cf8b74e8bc137a9bbad4c0de3cdff844f1d
PrathamKapoor <prathamkapoor027@gmail.com>
2026-09-12 06:59:12 +0530

    mount overlays, minimap snapshot

--- src/components/sites/www-contentarchitecture-dev-80b3abaf/root-8a5edab2/ContentArchitecturePage.tsx
+++ src/components/sites/www-contentarchitecture-dev-80b3abaf/root-8a5edab2/ContentArchitecturePage.tsx
@@ -4,7 +4,10 @@ import { PageShell } from "../shared/PageShell";
 import { BannerSection } from "./BannerSection";
 import { FaqSection } from "./FaqSection";
 import { FeaturesSection } from "./FeaturesSection";
+import { FloatingCapture } from "./FloatingCapture";
 import { HeroSection } from "./HeroSection";
+import { LearnMore } from "./LearnMore";
+import { Minimap } from "./Minimap";
 import { PricingSection } from "./PricingSection";
 import { ProblemsSection } from "./ProblemsSection";
 import { RepoSection } from "./RepoSection";
@@ -15,8 +18,10 @@ import { SiteNav } from "./SiteNav";
 import { bannerContent } from "./content/banner";
 import { faqContent } from "./content/faq";
 import { featuresContent } from "./content/features";
+import { floatingCaptureContent } from "./content/floating-capture";
 import { footerContent } from "./content/footer";
 import { heroContent } from "./content/hero";
+import { learnMoreContent } from "./content/learn-more";
 import { navContent } from "./content/nav";
 import { pricingContent } from "./content/pricing";
 import { problemsContent } from "./content/problems";
@@ -27,7 +32,17 @@ import { showcaseContent } from "./content/showcase";
 /** contentarchitecture.dev, assembled from the section components and their CA content. */
 export function ContentArchitecturePage() {
   return (
-    <PageShell header={<SiteNav content={navContent} />} footer={<SiteFooter content={footerContent} />}>
+    <PageShell
+      header={<SiteNav content={navContent} />}
+      footer={<SiteFooter content={footerContent} />}
+      overlays={
+        <>
+          <Minimap />
+          <LearnMore content={learnMoreContent} />
+          <FloatingCapture content={floatingCaptureContent} />
+        </>
+      }
+    >
       <HeroSection content={heroContent} />
       <ProblemsSection content={problemsContent} />
       <FeaturesSection content={featuresContent} />
